6a2759f0514c29330280d42844d4649028277539
bengrobstein <bengrobstein@bens-mbp-2.dhcp.wustl.edu>
2017-10-09 15:54:33 -0500

    studio 6

--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -2,7 +2,9 @@ package studio6;
 
 import jssc.*;
 
+
 public class SerialComm {
+
 
 	SerialPort port;
 
@@ -29,8 +31,32 @@ public class SerialComm {
 	// TODO: Add writeByte() method from Studio 5
 	
 	// TODO: Add available() method
+	public boolean available() throws SerialPortException {
+		if (port.getInputBufferBytesCount() > 0) {
+			return true;
+		}
+		return false;
+	}
 	
 	// TODO: Add readByte() method	
 	
+	public byte readByte() throws SerialPortException {
+		return port.readBytes()[0];
+	}
+	
 	// TODO: Add a main() method
-}
+	public static void main(String[] args) throws SerialPortException{ 
+		
+		SerialComm com = new SerialComm("/dev/cu.usbserial-DN02AZYQ");
+		while (true) {
+			if(com.available()) {
+				byte x = com.readByte();
+				System.out.print((char) x);
+			}
+			}
+		}
+	
+	}
+
+
+
